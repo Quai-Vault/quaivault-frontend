a1870b32712480df0265eb970f1c6a592e80c157
mpoletiek <matthew.poletiek@gmail.com>
2026-05-25 15:33:16 -0500

    mainnet changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -128,17 +128,6 @@ export function Layout({ children }: LayoutProps) {
               </Link>
             </div>
             <div className="flex items-center gap-4">
-              {/* Testnet Warning Badge */}
-              <div
-                className="hidden sm:flex items-center gap-2 px-3 py-1 rounded-full bg-yellow-100 dark:bg-yellow-900/40 border border-yellow-300 dark:border-yellow-700/50"
-                title="This application is running on Orchard Testnet. Do not store significant funds."
-              >
-                <svg className="w-3.5 h-3.5 text-yellow-600 dark:text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                </svg>
-                <span className="text-xs font-semibold text-yellow-700 dark:text-yellow-300">Orchard Testnet</span>
-                <span className="text-xs text-yellow-600 dark:text-yellow-400 hidden md:inline">• Do not store significant funds</span>
-              </div>
               <NotificationToggle />
               <ThemeToggle />
               <a
